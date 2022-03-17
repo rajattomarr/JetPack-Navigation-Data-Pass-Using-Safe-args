8796940fa4d10e7088a583ee5d10f371d3cb3ae9
rajattomarr <rajattomarofficial@gmail.com>
2022-03-17 16:50:47 +0530

    Initial Level Commit

--- app/src/main/java/com/example/jetpack_navigation/chooserecepientFragment.kt
+++ app/src/main/java/com/example/jetpack_navigation/chooserecepientFragment.kt
@@ -1,13 +1,10 @@
 package com.example.jetpack_navigation
-
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
@@ -32,8 +29,9 @@ class chooserecepientFragment : Fragment(), View.OnClickListener{
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        navcontroller = Navigation.findNavController(view)
 
+
+        navcontroller = Navigation.findNavController(view)
         binding.nextBtn.setOnClickListener(this)
 
     }
